2a1e8e3c287d7599c41765baf3f5c657861bfe59
Aziz Ashurov <a.o.ashurov@gmail.com>
2020-05-20 04:03:24 +0500

    test commit

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
+
 <html lang="en" class="no-js">
-<!--<![endif]-->
 
 <head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
