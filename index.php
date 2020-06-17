6de1fd4bd85e854ce35e92be1f236e36f76a2534
Aziz Ashurov <a.o.ashurov@gmail.com>
2020-06-17 17:43:00 +0500

    commit message

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 <html lang="en" class="no-js">
 
 <head>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166769303-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-166769303-1');
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MX24DS3');</script>
+<!-- End Google Tag Manager -->
 </script>
 
 
@@ -58,7 +57,10 @@
 </head>
 
 <body id="body">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX24DS3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	<!-- preloader -->
 	<div id="preloader">
 		<img src="img/preloader.gif" alt="Preloader">
